e88b04efbc30c34de074b65cb18b684416cd672f
Alex Khlopiachyi <sashax91999@gmail.com>
2019-03-07 16:04:22 +0200

    Feature: add jest helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,3 +3,4 @@ export * from './convertToUTCTime';
 export * from './findPhone';
 export * from './sortTable';
 export * from './convertErrors';
+export * from './jest';
---
+++ src/helpers/jest.ts
@@ -0,0 +1,22 @@
+import Axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { Middleware } from 'redux';
+import configureMockStore from 'redux-mock-store';
+import { Config, Tower } from '../api/config';
+
+const mockConfig: Config = {
+    authUrl: '/api/v2/barong',
+    tablePageLimit: 50,
+    msAlertDisplayTime: '3000',
+};
+
+export const setupMockStore = (appMiddleware: Middleware, log = false) => configureMockStore([appMiddleware]);
+
+export const setupMockAxios = () => {
+    Tower.config = mockConfig;
+    return new MockAdapter(Axios);
+};
+
+export const mockNetworkError = (mockAxios: any) => {
+    mockAxios.onAny().networkError();
+};
